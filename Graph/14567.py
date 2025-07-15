228129da4d4bcd7162998245b6f313665039ee13
leeyueun1012 <leeyueun1012@naver.com>
2025-07-15 20:15:49 +0900

    "20250715"

--- Graph/14567.py
+++ Graph/14567.py
@@ -4,19 +4,9 @@ from collections import deque
 n,m = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n+1)]            # 노드 연결 그래프
 degree = [1]*(n+1)    # 진입 차수
-visited = [False]*(n+1)  # 방문 여부
+queue = deque()
 
 for _ in range(m):
     a,b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
-
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            if degree[v] >= degree[i]:
-                degree[i] += degree[v]
-            dfs(i)    
-
-dfs(1)
-print(*degree[1:])         
+    
